c58651864746378686c664cfbb98428f7958cc79
Jahoosher <josh.massey1202@gmail.com>
2024-02-29 15:33:43 +0100

    Guided project complete

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ def main():
     text = get_book_text(book_path)
     word_count = count_words(text)
     letter_count = count_letters(text)
+    sorted_report = get_report(letter_count)
+
+    print(f" --- Begin report of {book_path} ---")
     print(f"{word_count} words found in the document!")
-    print(f"Here is how many times each character was used in this book: {letter_count}")
+    print()
+
+    for item in sorted_report:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item["num"]} times")
+
+    print("--- End report ---")
+
     
 
 def get_book_text(path):
@@ -25,4 +36,15 @@ def count_letters(text):
                 letters[char] = 1
     return letters
 
+
+def sort_on(d):
+    return d["num"]
+
+def get_report(letter_count):
+    sorted_letters = []
+    for ch in letter_count:
+        sorted_letters.append({"char": ch, "num": letter_count[ch]})
+    sorted_letters.sort(reverse=True, key=sort_on)
+    return sorted_letters
+    
 main()
